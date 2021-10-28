47421f410bf3c72e79c6afdb3520646214732f23
Shayan <s089941@ap.Be>
2021-10-28 19:47:11 +0200

    Query data met GraphQL finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: "Artist Agency",
-    description: "Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry.",
+    title: "Shayan the Artis Agency",
+    description:
+      "Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry.",
     author: "@gatsbyjs",
     siteUrl: "https://gatsbystarterdefaultsource.gatsbyjs.io/",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "http://shayan-agency.local/graphql",
+      },
+    },
   ],
-};
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <main>
-      <Layout pageTitle="Welcome to Inghelbrecht Agency!">
+      <Layout pageTitle="Welcome to Shayan Agency!">
       <p>Lorem ipsum</p>
       <StaticImage
         alt="Gotta cope with laif"
