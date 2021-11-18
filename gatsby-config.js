3ef5a49beac7190cfb8a5f88db8cb1f650428f1b
Shayan <s089941@ap.Be>
2021-11-18 19:51:10 +0100

    completed agency

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Shayan the Artis Agency",
+    title: "Shayan Khademi",
     description:
       "Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry.",
     author: "@gatsbyjs",
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-wordpress",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,115 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Artist from "../components/artist"
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  headerTitle,
+  CTA,
+  section,
+  subtitle,
+  artists,
+} from "../page.module.css"
 
-const IndexPage = () => {
+const IndexPage = ({
+  data: {
+    wpPage: { homePageFields },
+  },
+}) => {
+  const image = getImage(homePageFields.headerHome.picture.localFile)
   return (
-    <main>
-      <Layout pageTitle="Welcome to Shayan Agency!">
-      <p>Lorem ipsum</p>
-      <StaticImage
-        alt="Gotta cope with laif"
-        src="https://gumlet.assettype.com/afkgaming%2F2021-08%2F79649079-d0e7-4acd-853b-6a2b92797da3%2Fcopium_png.png?format=auto"
-      />
-      <StaticImage
-        alt="Pepe Is Sad"
-        src="../images/feelsbadman.png"
-      />
-      </Layout>
-    </main>
+    <Layout>
+      <div className={header}>
+        <div className={headerInfo}>
+          <h1 className={headerTitle}>{homePageFields.headerHome.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: homePageFields.headerHome.description,
+            }}
+          />
+          <a
+            className={CTA}
+            target="__blank"
+            href={homePageFields.callToAction.link}
+          >
+            {homePageFields.callToAction.description}
+          </a>
+        </div>
+        <div>
+          <GatsbyImage
+            image={image}
+            className={headerPicture}
+            alt={homePageFields.headerHome.picture.altText}
+          />
+        </div>
+      </div>
+      <div className={section}>
+    <h2 className={subtitle}>{homePageFields.featuredArtists.title}</h2>
+    <p>{homePageFields.featuredArtists.description}</p>
+    <div className={artists}>
+      {homePageFields.featuredArtists.artists.map(artist => (
+           <Artist
+              slug={`artists/${artist.slug}`}
+              key={artist.id}
+              artist={artist}
+            />
+      ))}
+    </div>
+  </div>
+</Layout>
   )
 }
-
+export const query = graphql`
+  query {
+    wpPage(slug: { eq: "home" }) {
+      homePageFields {
+        headerHome {
+          description
+          title
+          picture {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, width: 500, height: 500)
+              }
+            }
+          }
+        }
+        callToAction {
+          description
+          link
+        }
+        featuredArtists {
+          artists {
+            ... on WpArtist {
+              id
+              artistMeta {
+                artistName
+                firstName
+                lastName
+                profilePicture {
+                  altText
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(
+                        placeholder: BLURRED
+                        transformOptions: { grayscale: true }
+                      )
+                    }
+                  }
+                }
+              }
+              slug
+            }
+          }
+          description
+          title
+        }
+      }
+    }
+  }
+`
 export default IndexPage
